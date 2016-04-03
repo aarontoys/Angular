745226f8587b5cc59da3b191fa5011b01fdc7ba6
Aaron Toys <aarontoys@gmail.com>
2016-04-03 15:27:42 -0600

    finished ping-pong score keeper

--- w13-d5/events.js
+++ w13-d5/events.js
@@ -9,5 +9,56 @@ app.controller('MainController', function ($scope) {
   $scope.reverseWord = function (w) {
     $scope.word = w.split('').reverse().join('');
   }
-  return $scope.word;
+
+  $scope.players = [
+    {
+      id: 1,
+      score: 0,
+      serving: true,
+      winner: -1,
+      gamesWon: 0
+    },
+    {
+      id: 2,
+      score: 0,
+      serving: false,
+      winner: -1,
+      gamesWon: 0
+    }
+  ];
+
+  var serveCount = 0;
+  $scope.addScore = function (player) {
+    player.score++;
+    serveCount++;
+    if (!(serveCount % 2) ) {
+      $scope.players[0].serving = !$scope.players[0].serving;
+      $scope.players[1].serving = !$scope.players[1].serving;
+    }
+    if (player.score === 11) {
+      $scope.newGame = 1;
+      player.gamesWon++;
+      player.winner += 2;
+      if (player.id === 1) {
+        $scope.players[1].winner++;
+      } else {
+        $scope.players[0].winner++;
+      }
+    }
+  }
+
+  $scope.startNewGame = function () {
+    $scope.players[0].score = 0;
+    $scope.players[0].serving = true;
+    $scope.players[0].winner = -1;
+    $scope.players[1].score = 0;
+    $scope.players[1].serving = false;
+    $scope.players[1].winner = -1;
+    serveCount = 0;
+  }
+  $scope.reset = function () {
+    $scope.startNewGame();
+    $scope.players[0].gamesWon = 0;
+    $scope.players[1].gamesWon = 0;
+  }
 });
